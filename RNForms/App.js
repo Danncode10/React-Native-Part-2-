8cb40aaeb154d76cd706be136203b32d80879ff3
Dann <Lesterdannlopez7@gmail.com>
2025-07-26 12:58:37 +0800

    Add dark mode toggle functionality and update UI components

--- RNForms/App.js
+++ RNForms/App.js
@@ -1,13 +1,41 @@
 import "./global.css"
-import { Text, View, SafeAreaView } from "react-native";
-import Login from './components/Login.js';
+import { Text, View, SafeAreaView, Switch, StatusBar} from "react-native";
+import { use, useState } from "react";
  
 export default function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [bgColor, setBgColor] = useState("bg-blue-100");
+  const [primaryColor, setPrimaryColor] = useState("bg-blue-800");
+  const [primaryColorText, setPrimaryColorText] = useState("text-blue-100");
+  const [barStyle, setBarStyle] = useState("dark-content");
+
+  function onToggleDarkMode(newValue){
+    setIsDarkMode(newValue);
+    setBgColor(isDarkMode? "bg-blue-100" : "bg-black");
+    setPrimaryColor(isDarkMode? "bg-blue-800": "bg-blue-300");
+    setPrimaryColorText(isDarkMode? "text-blue-300": "text-blue-800");
+    setBarStyle(isDarkMode? "dark-content": "light-content");
+  }
+
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className={`flex-1 items-center justify-center ${bgColor}`}>
+      <StatusBar barStyle={barStyle} />
       <SafeAreaView className="flex-1 w-full p">
-        <View className="border border-red-500 flex-1 justify-center p-4">
-           <Login />
+        <View className=" flex-1 p-4 ">
+          <View className={`${primaryColor} justify-between flex-row rounded-full p-4`}>
+            <Text className={`text-2xl font-bold ${primaryColorText}`}>Dark Mode</Text>
+           <Switch 
+            value={isDarkMode}
+            onValueChange={onToggleDarkMode}
+            trackColor={{true: "blue"}}
+            thumbColor={isDarkMode? "lightblue": "white"}
+            ios_backgroundColor="lightblue"
+            
+           />
+          </View>
+           
         </View>
         
       </SafeAreaView>
